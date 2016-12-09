1ae8269f657e1145eb3d9ed8c9c575c82a736fe9
ucapdak <ucapdak@ucl.ac.uk>
2016-12-09 23:01:52 +0000

    changed variables

--- loewnercode/looptestinfortran.f90
+++ loewnercode/looptestinfortran.f90
@@ -10,15 +10,15 @@ implicit none
 
     real, parameter :: T = 1.0      !
     integer, parameter :: NN = 500  !
-    real :: delta = T / NN          !
+    real :: twodelta = 2 * (T / NN) !
     integer :: j                    ! Loop counter
     complex :: g_T1 = 0.            ! 
     complex :: g_T2 = 0.            !
-    complex :: i = complex(0,1)     ! Imaginary number
+    complex :: i = complex(0,1)     ! Imaginary unit
     
     do j = 1, NN
     
-        g_T2 = (g_T1 / 2) + i * sqrt((2 * delta) - ((g_T1 ** 2) / 4))
+        g_T2 = (g_T1 / 2) + i * sqrt(twodelta - ((g_T1 ** 2) / 4))
         print *, g_T2
         g_T1 = g_T2
         

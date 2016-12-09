cc2262154ee4b73b38192f80e4f4e7fa653730e7
ucapdak <ucapdak@ucl.ac.uk>
2016-12-09 23:30:08 +0000

    license

--- loewnercode/looptestinfortran.f90
+++ loewnercode/looptestinfortran.f90
@@ -8,17 +8,17 @@
 program Loewner
 implicit none
 
-    real, parameter :: T = 1.0      !
-    integer, parameter :: NN = 500  !
-    real :: twodelta = 2 * (T / NN) !
-    integer :: j                    ! Loop counter
-    complex :: g_T1 = 0.            ! 
-    complex :: g_T2 = 0.            !
-    complex :: i = complex(0,1)     ! Imaginary unit
+    real, parameter :: T = 1.0       !
+    integer, parameter :: NN = 500   !
+    real :: two_delta = 2 * (T / NN) !
+    integer :: j                     ! Loop counter
+    complex :: g_T1 = 0.             ! 
+    complex :: g_T2 = 0.             !
+    complex :: i = complex(0,1)      ! Imaginary unit
     
     do j = 1, NN
     
-        g_T2 = (g_T1 / 2) + i * sqrt(twodelta - ((g_T1 ** 2) / 4))
+        g_T2 = (g_T1 / 2) + i * sqrt(two_delta - ((g_T1 ** 2) / 4))
         print *, g_T2
         g_T1 = g_T2
         

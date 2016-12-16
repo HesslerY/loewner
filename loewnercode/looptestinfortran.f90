140193ac76e5e5d3615581a55d5421e496c33947
ucapdak <ucapdak@ucl.ac.uk>
2016-12-16 22:09:35 +0000

    outerloop

--- loewnercode/looptestinfortran.f90
+++ loewnercode/looptestinfortran.f90
@@ -8,20 +8,31 @@
 program Loewner
 implicit none
 
-    real, parameter :: T = 1.0       !
-    integer, parameter :: NN = 500   !
-    real :: two_delta = 2 * (T / NN) !
-    integer :: j                     ! Loop counter
-    complex :: g_T1 = 0.             ! 
-    complex :: g_T2 = 0.             !
-    complex :: i = complex(0,1)      ! Imaginary unit
+    real, parameter :: step = 0.1               !
+    integer, parameter :: NN = 500              !
+    real :: two_delta  = 0.0                    !
+    integer :: j                                ! Loop counter
+    integer :: k                                !
+    real :: T                                   !
+    complex :: g_T1 = 0.                        ! 
+    complex :: g_T2 = 0.                        !
+    complex, parameter :: i = complex(0,1)      ! Imaginary unit
+
+    do j = 1,100
+
+        T = j * step
+        two_delta = 2 * (T / NN)
+        ! print *, T
     
-    do j = 1, NN
+        do k = 1, NN
     
-        g_T2 = (g_T1 / 2) + i * sqrt(two_delta - ((g_T1 ** 2) / 4))
-        print *, g_T2
-        g_T1 = g_T2
+            g_T2 = (g_T1 / 2) + i * sqrt(two_delta - ((g_T1 ** 2) / 4))
+            g_T1 = g_T2
         
+        end do
+
+        print *, g_T2
+
     end do
 
 end program Loewner

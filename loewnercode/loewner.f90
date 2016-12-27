efc7fc319192ef288af3e6f3dece19f37c5d6cd0
ucapdak <ucapdak@ucl.ac.uk>
2016-12-27 13:44:53 +0000

    complex8

--- loewnercode/loewner.f90
+++ loewnercode/loewner.f90
@@ -5,10 +5,14 @@
 ! Author: Dolica Akello-Egwel                                          !
 !                                                                      !
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
 function square(i) result(j)
-    complex :: i ! input
-    complex :: j ! output
+
+    complex(8) :: i ! Argument
+    complex(8) :: j ! Return value
+    
     j = i ** 2
+    
 end function square
  
 program Loewner
@@ -17,15 +21,17 @@ implicit none
     integer, parameter :: NN = 500              !
     real, parameter :: step = 0.1               !
     real :: two_delta                           !
-    integer :: j                                ! Loop counter
-    integer :: k                                !
-    real :: T                                   !
-    complex :: g_T1                             ! 
-    complex :: g_T2                             !
+    integer :: j = 0                            ! Loop counter
+    integer :: k = 0                            !
+    real :: T = 0                               !
+    complex(8) :: g_T1                          ! 
+    complex(8) :: g_T2                          !
     complex, parameter :: i = complex(0,1)      ! Imaginary unit
     real, parameter :: pi = 3.1415927
-    real :: driv_func
-    complex :: square
+    complex(8) :: drive_func                    ! Driving function
+    
+    complex(8) :: square                        ! Compute x^2
+    ! real :: get_driving_value                 ! Obtain driving value
 
     ! Open the output file
     open(unit = 1, file = "result.txt")
@@ -34,16 +40,16 @@ implicit none
 
         T = T + step
         two_delta = 2 * (T / NN)
-        driv_func = 0
+        drive_func = T * cos(T * pi)
         
         do k = 1, NN
     
-            g_T2 = ((g_T1 + driv_func) / 2) + i * sqrt(two_delta + (g_T1 * driv_func) - (square(g_T1 - driv_func) / 4))
+            g_T2 = ((g_T1 + drive_func) / 2) + i * cdsqrt(two_delta + (g_T1 * drive_func) - (square(g_T1 - drive_func) / 4))
             g_T1 = g_T2
         
         end do
 
-        write (1,*) g_T2
+        write (1,*) real(g_T2), imag(g_T2)
 
     end do
 
--- loewnercode/plot.py
+++ loewnercode/plot.py
@@ -8,20 +8,20 @@ results_file = open('result.txt', 'r')
 x_values = []
 y_values = []
 
+smooth_curve = False
+
 for line in results_file:
-    nobrackets = (line.replace("(","")).replace(")","")
-    values = nobrackets.split(",")
+
+    values = line.split()
 
     x_values.append(float(values[0]))
     y_values.append(float(values[1]))
 
-
 # x_values = nan_to_num(x_values)
 # y_values = nan_to_num(y_values)
 
-
 # Don't smooth curve in case of a straight line
-if x_values.count(x_values[0]) is not len(x_values):
+if smooth_curve:
     xnew = linspace(min(x_values),max(x_values),50)
     y_smooth = spline(x_values,y_values,xnew)
 
@@ -29,6 +29,9 @@ if x_values.count(x_values[0]) is not len(x_values):
     plt.show()
 
 plt.plot(x_values,y_values)
+plt.show()
+
+plt.scatter(x_values,y_values)
 plt.show()
 
 

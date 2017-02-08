64b28bcc59e5bf5542a782d26ce7cef33ebb3712
ucapdak <ucapdak@ucl.ac.uk>
2017-02-08 22:27:11 +0000

    made script for preparing output

---
+++ loewnercode/plottopdf.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+latex_file="plots.tex"
+output_folder=./output/*.png
+
+# Delete the previous file
+rm -r "$latex_file"
+
+# Write something to the latex file
+function write_latex_file()
+{
+    echo "$1" >> "$latex_file"
+}
+
+function begin_image()
+{
+    write_latex_file "\begin{figure}[t]"
+    write_latex_file "\centering"
+}
+function end_image()
+{
+    write_latex_file "\end{figure}"
+}
+function first_picture()
+{
+    begin_image
+    write_latex_file "$1"
+}
+function second_picture()
+{
+    write_latex_file "$1"
+    end_image
+}
+
+# Write the preamble to the latex file
+write_latex_file "\documentclass[a4paper, 12pt]{article}"
+write_latex_file "\usepackage[letterpaper, portrait, margin=0.8in]{geometry}"
+write_latex_file "\usepackage{graphicx}"
+write_latex_file "\usepackage[space]{grffile}"
+write_latex_file "\graphicspath{ {output/} }"
+write_latex_file "\begin{document}"
+
+for f in $output_folder; do
+
+    if [[ $f == *"[scatter]"* ]]; then
+    
+        second_picture "\includegraphics[width=0.8\textwidth]{${f:9}}"
+        
+    else
+        
+        first_picture "\includegraphics[width=0.8\textwidth]{${f:9}}"
+        
+    fi
+
+done
+
+# Write the final part of the latex file
+write_latex_file "\end{document}"
+
+# Compile the latex file
+# pdflatex plots.tex

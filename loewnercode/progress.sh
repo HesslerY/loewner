a54bacc5f107165dfcf011ea4e764b02fe4ee385
ucapdak <ucapdak@ucl.ac.uk>
2017-03-02 15:34:29 +0000

    progress only 2dp

--- loewnercode/progress.sh
+++ loewnercode/progress.sh
@@ -5,7 +5,7 @@ total_lines=$(wc -l < result.txt)
 echo "Enter number of iterations:"
 read n_iterations
 
-prog=$(echo "print($total_lines/$n_iterations * 100)" | python)
+prog=$(echo "print(str($total_lines/$n_iterations * 100)[:5])" | python)
 
 echo "$total_lines/$n_iterations"
 echo $prog "%"

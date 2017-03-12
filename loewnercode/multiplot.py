9af5d61ab0445f60f956fd83aefe02186dff0bc5
ucapdak <ucapdak@ucl.ac.uk>
2017-03-12 21:44:09 +0000

    multiplot works for all driving functions

--- loewnercode/multiplot.py
+++ loewnercode/multiplot.py
@@ -7,19 +7,29 @@ colour_options = ["b","g","r","k","m"]
 # Label of number of iterations for each of the different plots
 labels = ["100","1000","10000","100000","200000"]
 
-titles = ["$\\xi (t) = 2 \ \sqrt{1 \ (1 - t)}$",
-          "$\\xi (t) = 2 \ \sqrt{2.5 \ (1 - t)}$",
-          "$\\xi (t) = 2 \ \sqrt{3.5 \ (1 - t)}$",
-          "$\\xi (t) = 2 \ \sqrt{4 \ (1 - t)}$",
-          "$\\xi (t) = 2 \ \sqrt{4.5 \ (1 - t)}$",
-          "$\\xi (t) = 2 \ \sqrt{6 \ (1 - t)}$",
-          "$\\xi (t) = 2 \ \sqrt{8 \ (1 - t)}$"]
+all_titles = ["$\\xi (t) = 0$",
+              "$\\xi (t) = t$",
+              "$\\xi (t) = \cos(t)$",
+              "$\\xi (t) = \cos(\pi t)$",
+              "$\\xi (t) = t \ \cos(t)$",
+              "$\\xi (t) = t \ \cos(\pi t)$",
+              "$\\xi (t) = \sin(t)$",
+              "$\\xi (t) = \sin(\pi t)$",
+              "$\\xi (t) = t \ \sin(t)$",
+              "$\\xi (t) = t \ \sin(\pi t)$",
+              "$\\xi (t) = 2 \ \sqrt{1 \ (1 - t)}$",
+              "$\\xi (t) = 2 \ \sqrt{2.5 \ (1 - t)}$",
+              "$\\xi (t) = 2 \ \sqrt{3.5 \ (1 - t)}$",
+              "$\\xi (t) = 2 \ \sqrt{4 \ (1 - t)}$",
+              "$\\xi (t) = 2 \ \sqrt{4.5 \ (1 - t)}$",
+              "$\\xi (t) = 2 \ \sqrt{6 \ (1 - t)}$",
+              "$\\xi (t) = 2 \ \sqrt{8 \ (1 - t)}$"]
 
-title_index = int(argv[1]) - 10
+title_index = int(argv[1])
 
-dir_name = "backup/" + str(title_index + 10) + "/"
+dir_name = "backup/" + str(title_index) + "/"
 
-plt.title(titles[title_index],fontsize = 18, color='black', y = 1.03)
+plt.title(all_titles[title_index],fontsize = 18, color='black', y = 1.03)
 
 # Iterate over the results files
 for i in range(5):
@@ -28,8 +38,10 @@ for i in range(5):
     try:
         filename = dir_name + str(i) + ".txt"
         result_file = open(filename,"r")
+        print("Opened a file!")
 
     except FileNotFoundError:
+        print("Unable to open " + filename)
         break
     
     # Create empty lists

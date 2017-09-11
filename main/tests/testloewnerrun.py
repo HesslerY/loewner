d939a188dcb662b083614efd9c200afe2f6a322a
ucapdak <ucapdak@ucl.ac.uk>
2017-09-11 19:28:54 +0100

    Added compilation test

--- main/tests/testloewnerrun.py
+++ main/tests/testloewnerrun.py
@@ -1,4 +1,5 @@
-import os
+from os import remove
+from glob import glob
 import sys
 import inspect
 import unittest
@@ -48,5 +49,26 @@ class LoewnerRunTests(unittest.TestCase):
             else:
                 self.assertTrue(fullmatch("_\d\d", loewner_run.module_name[-3:]))
 
+    def test_compile_command(self):
+
+        loewner_runs = self.create_loewner_runs()
+        module_file_pattern = "modules/*.so"
+
+        for module_file in glob(module_file_pattern):
+            remove(module_file)
+
+        total_module_files = len(glob(module_file_pattern))
+
+        self.assertTrue(total_module_files == 0)
+
+        for loewner_run in loewner_runs:
+            loewner_run.set_compile_command()
+            loewner_run.compile_loewner()
+
+        total_module_files = len(glob(module_file_pattern))
+        total_loewner_runs = len(loewner_runs)
+
+        self.assertTrue(total_module_files == total_loewner_runs)
+            
 if __name__ == '__main__':
     unittest.main()

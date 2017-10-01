88340f2e34b535959595fad9ab53ab08d5c6f824
ucapdak <ucapdak@ucl.ac.uk>
2017-10-01 20:59:31 +0100

    Basic menu switching

--- main/Menu.py
+++ main/Menu.py
@@ -1,40 +1,71 @@
 import npyscreen
 import curses
+import Constants
 
 class RunLoewner(npyscreen.NPSAppManaged):
 
     def onStart(self):
 
         self.addForm("MAIN", LoewnerOptions, name="Main")
+        self.addForm("STANDARD", DrivingOptions, name="Select the driving function(s):")
+
+    def change_form(self, name):
+
+        self.switchForm(name)
 
 class LoewnerOptions(npyscreen.ActionForm):
 
     def create(self):
 
-        self.option = self.add(npyscreen.TitleSelectOne, max_height=4, value = [1,], name="Pick One",
-                values = ["Option1","Option2","Option3"], scroll_exit=True)
+        self.option = self.add(npyscreen.TitleSelectOne, max_height=3, value = [1,], name="Pick One",
+                values = ["Standard Mode","Option2","Option3"], scroll_exit=True)
 
     def on_ok(self):
 
-        if str(self.option.get_selected_objects()) == "['Opt1']":
-            change_forms()
+        selection = self.option.get_selected_objects()[0]
+        self.change_forms(selection)
 
     def on_cancel(self):
 
         self.parentApp.switchForm(None)
 
-    def change_forms(self, *args, **keywords):
+    def change_forms(self, selection):
 
-        if self.name == "Main":
-            change_to = "SECOND"
-        elif self.name == "Screen 2":
-            change_to = "THIRD"
+        if selection == "Standard Mode":
+            change_to = "STANDARD"
+        elif selection == "Exact Solutions":
+            change_to = "EXACT"
         else:
             change_to = "MAIN"
 
-        # Tell the MyTestApp object to change forms.
         self.parentApp.change_form(change_to)
 
+class DrivingOptions(npyscreen.ActionForm):
+
+    def create(self):
+
+        self.option = self.add(npyscreen.TitleSelectOne, max_height=3, value = [1,], name="Pick One",
+                values = ["a","b","c"], scroll_exit=True)
+
+    def on_ok(self):
+
+        selection = self.option.get_selected_objects()[0]
+        self.change_forms(selection)
+
+    def on_cancel(self):
+
+        self.parentApp.switchForm(None)
+
+    def change_forms(self, selection):
+
+        if selection == "Standard Mode":
+            change_to = "STANDARD"
+        elif selection == "Exact Solutions":
+            change_to = "EXACT"
+        else:
+            change_to = "MAIN"
+
+        self.parentApp.change_form(change_to)
 if __name__ == "__main__":
     App = RunLoewner()
     App.run()

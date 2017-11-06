04b88da570c42e15f0f4a42940a50a7d8cf4e790
ucapdak <ucapdak@ucl.ac.uk>
2017-11-06 22:01:14 +0000

    plots for report

--- main/Plot.py
+++ main/Plot.py
@@ -5,50 +5,56 @@ from numpy import arctan2
 from os.path import dirname, exists
 from os import mkdir
 
-plt.style.use('bmh')
+plt.style.use('seaborn')
 
 class Plot:
 
     def __init__(self, driving_function, resolution_parameters, results):
 
         self.results = results
-
+        self.driving_function = driving_function
         # Assign the plot title
         self.output_plot_title = Constants.PLOT_TITLE[driving_function]
-    
+
         # Determine the output directory for the plot images
-        self.output_plot_directory = "outputimages/" + str(driving_function) + "/"
-        
+        self.output_plot_directory = "/home/dolica/Documents/writeuploewner/finalreport/images/"
+
         # Determine the partial filename for the plot images
         self.partial_output_filename = str(driving_function) + " " + "-".join([str(x) for x in resolution_parameters])
-        
+
         # Display or save plot
-        self.display = True
-        
+        self.display = False
+
         # Assign the run paramters
         self.resolution_parameters = resolution_parameters
-        
+
     def create_output_folder(self):
-        
+
         image_directory = dirname(self.output_plot_directory)
-        
+
         if not exists(image_directory):
             mkdir(image_directory)
-        
+
     def generate_plot(self):
 
         # Set the plot title
         plt.title(self.output_plot_title, fontsize = 19, color = "black", y = 1.02, usetex = True)
-        
+
         # Plot the values
-        plt.plot(self.results.real, self.results.imag)
+        plt.plot(self.results.real, self.results.imag, color='crimson')
 
         if self.display:
             plt.show()
-            
+
         else:
-            self.create_output_folder()
-            plt.savefig(self.output_plot_directory + self.partial_output_filename + ".png")
+
+            if self.driving_function == 0:
+                plt.xticks(self.results.real, ["","","0","",""])
+
+            plt.xlabel('Re(g)')
+            plt.ylabel('Im(g)')
+            plt.ylim(bottom=0)
+            plt.savefig(self.output_plot_directory + self.partial_output_filename + ".pdf")
 
     def generate_scatter_plot(self):
         pass

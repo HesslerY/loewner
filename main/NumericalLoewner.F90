3f98d1ed1c641ade2e87f07292577c9354fc1a9e
ucapdak <ucapdak@ucl.ac.uk>
2018-02-08 17:12:19 +0000

    removing

--- main/NumericalLoewner.F90
+++ main/NumericalLoewner.F90
@@ -11,9 +11,20 @@
 program test
 end program test
 
-module poly3zerosModule
+module Constants
+implicit none
+
+   real(8), parameter :: pi = 4. * atan(1.)
+   real(8) :: sqrt_param = 0.0
+   complex, parameter :: i = complex(0,1)
+   complex(8), parameter :: ZERO = complex(0,0)
+   complex(8), parameter :: a = 1
+
+end module Constants
+
+module CubicSolver
   implicit none
-  public :: CubicRoot, zroot3, eq
+  public :: CubicRoot
   real :: PI = 3.14159265359
   real :: EPS = 1e-3
   real :: DELTA = 1e-5
@@ -28,50 +39,14 @@ contains
     real(8) :: AA, BB
     integer :: i, j
 
-    d = polym_coeffs(1)
-    a1 = polym_coeffs(2)
-    b1 = polym_coeffs(3)
-    c1 = polym_coeffs(4)
-
-    a = a1 / d
-    b = b1 / d
-    c = c1 / d
+    a = polym_coeffs(1)
+    b = polym_coeffs(2)
+    c = polym_coeffs(3)
 
     p = -a*a/3 + b
     q = 2*((a/3)**3) - a*b/3 + c
 
     qu = (p/3)**3 + (q/2)**2
-
-    As = zroot3(-q/2 + sqrt(qu))
-    Bs = zroot3(-q/2 - sqrt(qu))
-
-    do i = 1, 3, 1
-      AA = real(As(i))
-
-      do j = 1, 3, 1
-        BB = real(Bs(j))
-        z = AA*BB + p/3
-
-        if (isZero(z)) then
-          exit
-        end if
-      end do
-
-      if (isZero(z)) then
-        exit
-      end if
-    end do
-
-    if (isZero(qu) .or. real(qu) > 0 .and. isZero(complex(0, aimag(qu)))) then
-      poly3zeros(1) = AA + BB - a/3
-      poly3zeros(2) = complex(-(AA + BB)/2,  (AA - BB)/2 * sqrt(3.)) - a/3
-      poly3zeros(3) = complex(-(AA + BB)/2, -(AA - BB)/2 * sqrt(3.)) - a/3
-    else
-      alpha = acos(-q/(2 * sqrt(-(p/3)**3))) / 3
-      poly3zeros(1) =  2 * sqrt(-p/3) * cos(alpha) - a/3
-      poly3zeros(2) = -2 * sqrt(-p/3) * cos(alpha + PI/3) - a/3
-      poly3zeros(3) = -2 * sqrt(-p/3) * cos(alpha - PI/3) - a/3
-    end if
 
     CubicRoot = poly3zeros(1)
 
@@ -89,88 +64,7 @@ contains
 
   end function
 
-  function isZero(x)
-    complex(8) :: x
-    logical :: isZero
-    isZero = abs(real(x)) < DELTA .and. abs(aimag(x)) < DELTA
-  end function
-
-  function zroot3(x)
-    complex(8), dimension(3) :: zroot3
-    complex(8) :: x
-    real(8) :: radius, theta
-
-    if (x == complex(0,0)) then
-      zroot3(1) = complex(0,0)
-      zroot3(2) = complex(0,0)
-      zroot3(3) = complex(0,0)
-      return
-    end if
-
-    if (real(x) == 0) then
-      if (aimag(x) > 0) then
-        theta = PI/2
-      else
-        theta = 3./2 * PI
-      end if
-    else if (aimag(x) == 0) then
-      if (real(x) > 0) then
-        theta = 0
-      else
-        theta = PI
-      end if
-    else
-      if (real(x) > 0) then
-        theta = atan(aimag(x) / real(x))
-      else
-        theta = PI + atan(aimag(x) / real(x))
-      end if
-    end if
-
-    radius = abs(x)**(1./3)
-    theta = theta / 3
-
-    zroot3(1) = radius * complex(cos(theta),             sin(theta))
-    zroot3(2) = radius * complex(cos(theta + 2./3 * PI), sin(theta + 2./3 * PI))
-    zroot3(3) = radius * complex(cos(theta + 4./3 * PI), sin(theta + 4./3 * PI))
-  end function
-
-  function eq(a, b)
-    implicit none
-    LOGICAL :: eq
-    complex, dimension(3) :: a, b
-    integer :: i, j
-    integer, dimension(3) :: used
-
-    used = (/0, 0, 0/)
-
-    do i = 1, 3, 1
-      do j = 1, 3, 1
-        if (used(j) == 0 .AND. abs(a(i) - b(j)) < EPS) then
-          used(j) = 1
-          exit
-        end if
-      end do
-    end do
-
-    eq = .TRUE.
-
-    do i = 1, 3, 1
-      eq = eq .AND. used(i) == 1
-    end do
-  end function
-
-end module poly3zerosModule
-module constants
-implicit none
-
-   real(8), parameter :: pi = 4. * atan(1.)
-   real(8) :: sqrt_param = 0.0
-   complex, parameter :: i = complex(0,1)
-   complex(8), parameter :: ZERO = complex(0,0)
-   complex(8), parameter :: a = 1
-
-end module constants
+end module CubicSolver
 
 pure function square(x) result(y)
 

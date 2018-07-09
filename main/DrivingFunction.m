96097c7b65caccc21a4ed4fb733df665c14b2154
ucapdak <ucapdak@ucl.ac.uk>
2018-07-09 18:17:57 +0100

    Additional driving functions

--- main/DrivingFunction.m
+++ main/DrivingFunction.m
@@ -8,8 +8,29 @@ function drivingFunction = DrivingFunction(drivingNumber)
             drivingFunction.xi = @(t) t;
             drivingFunction.name = '\xi(t) = t';
         case 3
-            drivingFunction.xi = @(t) 1;
-            drivingFunction.name = '\xi(t) = 1';
+            drivingFunction.xi = @(t) cos(t);
+            drivingFunction.name = '\xi(t) = cos(t)';
+        case 4
+            drivingFunction.xi = @(t) t * cos(t);
+            drivingFunction.name = '\xi(t) = t * cos(t)';
+        case 5
+            drivingFunction.xi = @(t) cos(t * pi);
+            drivingFunction.name = '\xi(t) = cos(t * \pi)';
+        case 6
+            drivingFunction.xi = @(t) t * cos(t * pi);
+            drivingFunction.name = '\xi(t) = t * cos(t * \pi)';
+        case 7
+            drivingFunction.xi = @(t) sin(t);
+            drivingFunction.name = '\xi(t) = sin(t)';
+        case 8
+            drivingFunction.xi = @(t) t * sin(t);
+            drivingFunction.name = '\xi(t) = t * sin(t)';
+        case 9
+            drivingFunction.xi = @(t) sin(t * pi);
+            drivingFunction.name = '\xi(t) = sin(t * \pi)';
+        case 10
+            drivingFunction.xi = @(t) t * sin(t * pi);
+            drivingFunction.name = '\xi(t) = t * sin(t * \pi)';
         otherwise
             disp('other value')
     end
--- main/WedgeLoewner.m
+++ main/WedgeLoewner.m
@@ -1,30 +1,30 @@
 % Discretise time interval
-N = 1000
+N = 2000
 tStart = 0
 tFinal = 25
 tRange = linspace(tStart,tFinal,N+1);
 deltaT = tFinal/N;
 
 % Set a value for alpha
-alpha = 3;
+alpha = 4;
 
 % Configure pi / alpha
-piOverAlpha = pi/alpha;
+piOverAlpha = pi - pi/alpha;
 
 % Set 'original' Loewner equation
-origLoewner = @(gt,gdt,drivingFunction) gt * gdt^(piOverAlpha) - gt * drivingFunction^(piOverAlpha) - gdt^(1 + piOverAlpha) + gdt*drivingFunction^(piOverAlpha) - 2*gdt*deltaT;
+origLoewner = @(gt,gdt,drivingFunction) gt * gdt^(piOverAlpha) - gt * drivingFunction^(piOverAlpha) - gdt*gdt^(piOverAlpha) + gdt*drivingFunction^(piOverAlpha) - 2*gdt*deltaT;
 
-% Define driving function
-xi = @(t) t*sin(t*pi);
+% Select a driving function
+df = DrivingFunction(6);
 
-% Set g_tFinal to driving function
-gResult = [xi(0)];
+% Set g_tFinal to driving function at time 0
+gResult = [df.xi(0)];
 
 % Iterate to find solutions from g_tFinal-1 to g_0
 for i=2:length(tRange)
 
     % Obtain driving function for current value of t
-    drivingFunction = xi(tRange(i));
+    drivingFunction = df.xi(tRange(i));
 
     % Set Loewner to be a function of g at previous time value
     newLoewner = @(gdt) origLoewner(gResult(end),gdt,drivingFunction);
@@ -44,4 +44,4 @@ end
 
 % Plot result
 plot(gResult)
-title(strcat(strcat({'\xi(t) = 1 / \alpha = '},num2str(alpha))))
+title(strcat(strcat(df.name,{' / \alpha = '},num2str(alpha))))

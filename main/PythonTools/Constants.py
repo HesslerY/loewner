4e53b9a0b3d0d60f37644b253537e173134c9142
ucapdak <ucapdak@ucl.ac.uk>
2018-07-24 09:31:38 +0100

    Some directories and functions added to Constants

--- main/PythonTools/Constants.py
+++ main/PythonTools/Constants.py
@@ -14,7 +14,7 @@ DRIVING_INFO = ["0.0",
                 "floor(t)",
                 "floot(t) % 2"]
 
-PLOT_TITLE = ["$\\xi (t) = ",
+PLOT_TITLE = [["$\\xi (t) = ","$"],
               "$\\xi (t) = t$",
               "$\\xi (t) = \cos(t)$",
               "$\\xi (t) = t \ \cos(t)$",
@@ -24,30 +24,40 @@ PLOT_TITLE = ["$\\xi (t) = ",
               "$\\xi (t) = t \ \sin(t)$",
               "$\\xi (t) = \sin(\pi t)$",
               "$\\xi (t) = t \ \sin(\pi t)$",
-              "",
-              "",
+              ["$\\xi (t) = 2 \ \sqrt{","\ (1 - t)}$"],
+              ["$\\xi (t) = c_{","} \sqrt{t}$"],
               "$\\xi (t) = \lfloor t \\rfloor $",
               "$\\xi (t) = \lfloor t \\rfloor \ \\mathrm{mod} \ 2$"]
-
-# Inicides of the driving functions
-DRIVING_INDICES = {pair[1] : pair[0] for pair in enumerate(DRIVING_INFO)}
 
 # Obtain the total number of driving functions
 TOTAL_DRIVING_FUNCTIONS = len(DRIVING_INFO)
 
 # Indices for "special" driving functions
 KAPPA_IDX = 10
-C_ALPHA_IDX = 11
+CALPHA_IDX = 11
 CONST_IDX = 0
+LINR_IDX = 1
 PERIODIC_DRIVING = [i for i in range(2,10)]
 MULTIPLE_IDX = TOTAL_DRIVING_FUNCTIONS
 ALL_IDX = TOTAL_DRIVING_FUNCTIONS + 1
+
+def MAKE_CONSTANT_TITLE(constant):
+    return PLOT_TITLE[CONST_IDX][0] + str(constant) + PLOT_TITLE[CONST_IDX][1] 
+
+def MAKE_KAPPA_TITLE(kappa):
+    return PLOT_TITLE[KAPPA_IDX][0] + str(kappa)[:3] + PLOT_TITLE[KAPPA_IDX][1]
+
+def MAKE_CALPHA_TITLE(calpha):
+    return PLOT_TITLE[CALPHA_IDX][0] + str(calpha)[:3] + PLOT_TITLE[CALPHA_IDX][1]
+
+# Inicides of the driving functions
+DRIVING_INDICES = {pair[1] : pair[0] for pair in enumerate(DRIVING_INFO)}
 
 # Compilation related lists
 F2PY_FIRST = ["f2py", "-c"]
 
 def SQUAREROOT_DRIVING(driving_function):
-    return driving_function in [KAPPA_IDX, C_ALPHA_IDX]
+    return driving_function in [KAPPA_IDX, CALPHA_IDX]
 
 # Extension for data file output
 DATA_EXT = ".dat"
@@ -57,18 +67,22 @@ PLOT_EXT = ".pdf"
 FORTRAN_EXT = ".F90"
 
 # Extension for data fortran files
-DAT_PREC = "%.18f"
+DATA_PREC = "%.18f"
 
 # Names of the Forward files that are used to solve Loewner's equation
 FOR_LOEWNER = "ForwardLoewner"
 INV_LOEWNER = "InverseLoewner"
 
 # Directories for the data files
+EXACT_FORWARD_DATA_OUTPUT = "../Output/Data/Quadratic/ExactSolutions/" 
+EXACT_CUBIC_DATA_OUTPUT = "../Output/Data/Cubic/ExactSolutions/" 
 FORWARD_DATA_OUTPUT = "../Output/Data/Quadratic/Forward/"
 INVERSE_DATA_OUTPUT = "../Output/Data/Quadratic/Inverse/"
 CUBIC_DATA_OUTPUT = "../Output/Data/Cubic/Forward/"
 
 # Directories for the plot files
+EXACT_FORWARD_PLOT_OUTPUT = "../Output/Plots/Quadratic/ExactSolutions/" 
+EXACT_CUBIC_PLOT_OUTPUT = "../Output/Plots/Cubic/ExactSolutions/" 
 FORWARD_PLOT_OUTPUT = "../Output/Plots/Quadratic/Forward/"
 INVERSE_PLOT_OUTPUT = "../Output/Plots/Quadratic/Inverse/"
 CUBIC_PLOT_OUTPUT =  "../Output/Plots/Cubic/Forward/"

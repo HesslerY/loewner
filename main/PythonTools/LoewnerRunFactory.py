854ffb6488e56c30bb5fab4de46b830fb5ba727a
ucapdak <ucapdak@ucl.ac.uk>
2018-07-25 21:28:41 +0100

    fixed missing index for LoewnerRunFactory

--- main/PythonTools/LoewnerRunFactory.py
+++ main/PythonTools/LoewnerRunFactory.py
@@ -1,25 +1,67 @@
 from Constants import CONST_IDX, LINR_IDX, KAPPA_IDX, CALPHA_IDX, SQRTPLUS_IDX
 from LoewnerRun import LoewnerRun, ConstantLoewnerRun, LinearLoewnerRun, KappaLoewnerRun, CAlphaLoewnerRun, SqrtTPlusOneLoewnerRun
 
-def LoewnerRunFactory(index, start_time, final_time, outer_points, inner_points, compile_modules = True, save_plot = True, save_data = True, kappa = None, alpha = None, constant = None):
+class LoewnerRunFactory():
 
-    if index == CONST_IDX:
-        return ConstantLoewnerRun(constant,start_time,final_time,outer_points,inner_points,compile_modules,save_plot,save_data)
+    def __init__(self, start_time, final_time, outer_points, inner_points, compile_modules = True, save_plot = True, save_data = True):
 
-    if index == LINR_IDX:
-        return LinearLoewnerRun(start_time,final_time,outer_points,inner_points,compile_modules,save_plot,save_data)
+        self.start_time = start_time
+        self.final_time = final_time
+        self.outer_points = outer_points
+        self.inner_points = inner_points
 
-    if index == KAPPA_IDX:
+        self.compile_modules = compile_modules
+        self.save_plot = save_plot
+        self.save_data = save_data
 
-        if final_time > 1:
-            final_time = 1
+        self.standard_idxs = [i for i in range (1,10)] + [i for i in range(12,15)]
 
-        return KappaLoewnerRun(kappa=kappa,start_time=start_time,final_time=final_time,outer_points=outer_points,inner_points=inner_points,compile_modules=compile_modules,save_plot=save_plot,save_data=save_data)
+    def select_single_run(self,index,start_time=None,final_time=None,outer_points=None,inner_points=None,constant=None,kappa=None,alpha=None):
 
-    if index == CALPHA_IDX:
-        return CAlphaLoewnerRun(alpha,start_time,final_time,outer_points,inner_points,compile_modules,save_plot,save_data)
+        if start_time is None:
+            start_time = self.start_time
 
-    if index == SQRTPLUS_IDX:
-        return SqrtTPlusOneLoewnerRun(start_time,final_time,outer_points,inner_points,compile_modules,save_plot,save_data)
+        if final_time is None:
+            final_time = self.final_time
 
-    return LoewnerRun(index,start_time,final_time,outer_points,inner_points,compile_modules,save_plot,save_data)
+        if outer_points is None:
+            outer_points = self.outer_points
+
+        if inner_points is None:
+            inner_points = self.inner_points
+
+        if index == CONST_IDX:
+            return ConstantLoewnerRun(constant,start_time,final_time,outer_points,inner_points,self.compile_modules,self.save_plot,self.save_data)
+
+        if index == LINR_IDX:
+            return LinearLoewnerRun(start_time,final_time,outer_points,inner_points,self.compile_modules,self.save_plot,self.save_data)
+
+        if index == KAPPA_IDX:
+
+            if final_time > 1:
+                final_time = 1
+
+            return KappaLoewnerRun(kappa,start_time,final_time,outer_points,inner_points,self.compile_modules,self.save_plot,self.save_data)
+
+        if index == CALPHA_IDX:
+            return CAlphaLoewnerRun(alpha,start_time,final_time,outer_points,inner_points,self.compile_modules,self.save_plot,self.save_data)
+
+        if index == SQRTPLUS_IDX:
+            return SqrtTPlusOneLoewnerRun(start_time,final_time,outer_points,inner_points,self.compile_modules,self.save_plot,self.save_data)
+
+        return LoewnerRun(index,start_time,final_time,outer_points,inner_points,self.compile_modules,self.save_plot,self.save_data)
+
+    def create_standard_runs(self):
+        return [self.select_single_run(index=i) for i in self.standard_idxs]
+
+    def vary_kappa(self, kappas):
+        return [self.select_single_run(index=KAPPA_IDX, kappa=k) for k in kappas]
+
+    def vary_alpha(self, alphas):
+        return [self.select_single_run(index=CALPHA_IDX, alpha=a) for a in alphas]
+
+    def vary_inner_res(self, index, points, constant=None, kappa=None, alpha=None):
+        return [self.select_single_run(index=index, inner_points=p, constant=constant, kappa=kappa, alpha=alpha) for p in points]
+
+    def vary_final_time(self, index, times, constant=None, kappa=None, alpha=None):
+        return [self.select_single_run(index=index, final_time=t, constant=constant, kappa=kappa, alpha=alpha) for t in times]

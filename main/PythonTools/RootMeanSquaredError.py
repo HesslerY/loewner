bcce8d68c2567e8dd194d23c97008f00dde6dc03
ucapdak <ucapdak@ucl.ac.uk>
2018-07-26 16:51:04 +0100

    Root mean squared error function for cubic equation

--- main/PythonTools/RootMeanSquaredError.py
+++ main/PythonTools/RootMeanSquaredError.py
@@ -1,4 +1,4 @@
-from Constants import QUADRATIC_FORWARD_RMS, DATA_EXT, DATA_PREC
+from Constants import QUADRATIC_FORWARD_RMS, CUBIC_FORWARD_RMS, DATA_EXT, DATA_PREC, EXACT_CUBIC_CONSTANT
 from LoewnerRunFactory import LoewnerRunFactory
 from numpy import square, mean, array, savetxt, absolute
 from math import sqrt
@@ -58,6 +58,8 @@ class RootMeanSquaredError:
                 # Execute the approx solutions
                 approx_sol.quadratic_forward_loewner()
 
+                print("Finished solution with inner res = " + str(approx_sol.inner_points) + " for driving function " + str(approx_sol.name))
+
                 # Calculate the root mean squared error
                 rms = self.calculate_rms(exact_sol.exact_quadratic_forward, approx_sol.forward_results)
 
@@ -69,3 +71,50 @@ class RootMeanSquaredError:
 
             # Save the error values to the filesystem
             savetxt(filename, array(rms_list), fmt=DATA_PREC)
+
+    def cubic_forward_error(self, points=None):
+
+        # Create a list of driving functions that have an exact solution for the quadratic forward case
+        exact_solutions = self.rms_factory.create_exact_cubic()
+
+        # Use the resolutions that were created during class initialisation if no others are given
+        if points == None:
+            points = self.resolutions
+
+        # Iterate through the exact solutions
+        for exact_sol in exact_solutions:
+
+            # Declare empty lists for the error values
+            rms_list_a = []
+            rms_list_b = []
+
+            # Carry out the exact solution
+            exact_sol.exact_cubic_forward_loewner()
+
+            # Create a list of LoewnerRuns corresponding with exact solution that have different inner resolutions
+            approx_solutions = self.rms_factory.vary_inner_res(exact_sol.index,points, constant=EXACT_CUBIC_CONSTANT)
+
+            # Iterate through the approx solutions
+            for approx_sol in approx_solutions:
+
+                # Execute the approx solutions
+                approx_sol.cubic_forward_loewner()
+
+                print("Finished solution with inner res = " + str(approx_sol.inner_points) + " for driving function " + str(approx_sol.name))
+
+                # Calculate the root mean squared error
+                rms_a = self.calculate_rms(exact_sol.exact_cubic_sol_a, approx_sol.cubic_results_a)
+                rms_b = self.calculate_rms(exact_sol.exact_cubic_sol_b, approx_sol.cubic_results_b)
+
+                # Add the RMS value to the list
+                rms_list_a.append([approx_sol.inner_points, rms_a])
+                rms_list_b.append([approx_sol.inner_points, rms_b])
+
+            # Create a filename for the error values
+            filename_a = CUBIC_FORWARD_RMS + str(exact_sol.index) + "-RMS-A" + DATA_EXT
+            filename_b = CUBIC_FORWARD_RMS + str(exact_sol.index) + "-RMS-B" + DATA_EXT
+
+            # Save the error values to the filesystem
+            savetxt(filename_a, array(rms_list_a), fmt=DATA_PREC)
+            savetxt(filename_b, array(rms_list_b), fmt=DATA_PREC)
+

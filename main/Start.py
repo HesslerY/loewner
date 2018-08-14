56e06d2c34a25bf4c049960ef9d2fa110c549f0f
ucapdak <ucapdak@ucl.ac.uk>
2018-08-14 13:06:12 +0100

    more interfaceee

--- main/Start.py
+++ main/Start.py
@@ -1,47 +1,73 @@
-from PythonTools.Constants import LOEWNER_PROMPT, FORSINGLE_HELPMSG, INVSINGLE_HELPMSG, FORINV_HELPMSG, TWO_HELPMSG, WEDGE_HELPMSG, HELPMSG, DRIVING_LIST, BACK_COMMAND
-from PythonTools.LoewnerRunFactory import LoewnerRunFactory
+from PythonTools.Constants import LOEWNER_PROMPT, FORSINGLE_HELPMSG, INVSINGLE_HELPMSG, FORINV_HELPMSG, TWO_HELPMSG, WEDGE_HELPMSG, HELPMSG, DRIVING_LIST, BACK_COMMANDS, HELP_COMMANDS
+#from PythonTools.LoewnerRunFactory import LoewnerRunFactory
 from prompt_toolkit import PromptSession
 from prompt_toolkit.shortcuts import button_dialog
 
 # Print a help message
 def print_help_message(msg_type):
-    print(help_responses[msg_type])
+
+    help_file_loc = help_responses[msg_type]
+    help_file = open(help_file_loc)
+
+    for line in help_file:
+        print(line.strip('\n'))
+
+    print("")
     return
 
 # Exit the program
 def exit_loewner(msg_type):
     exit()
 
+def bad_input_message(user_input):
+    print("Unrecognised instruction: " + user_input + " (Press h for help)")
+
 def print_driving_functions():
 
     for item in DRIVING_LIST:
         print(item)
 
+def valid_driving_functions(user_input):
+
+    for item in user_input.split():
+
+        try:
+            temp = float(item)
+        except ValueError:
+            return False
+
+    return True
+
 # Run the forward single-trace algorithm
 def forward_single_trace():
 
-    print("Forward Single-Trace Selected.")
+    print("Forward Single-Trace Mode:")
 
     # Run the prompt
     while True:
 
-        # Print a message to the prompt
-        print("Choose driving function(s) or go [b]ack?")
-
-        # Print the driving functions
-        print_driving_functions()
-
         # Await under input
-        user_choice = session.prompt()
+        user_input = session.prompt()
 
         # Check for 'go back' instruction
-        if user_choice is in BACK_COMMANDS:
+        if user_input in BACK_COMMANDS:
             return
 
-        elif user_choice is in HELP_COMMANDS:
-            print_help_message("forwardsingle")
+        # check for the help instruction
+        elif user_input == "dr":
+            print_driving_functions()
 
-        else if
+        # Check if in the response correponds with help/exit
+        elif user_input in basic_responses:
+            basic_responses[user_input](user_input)
+
+        # Check if a list of driving functions were entered
+        elif valid_driving_functions(user_input):
+            pass
+
+        # Print the bad input message
+        else:
+            bad_input_message(user_input)
 
 # Run the inverse single-trace algorithm
 def inverse_single_trace():
@@ -49,57 +75,89 @@ def inverse_single_trace():
     print("Inverse Single-Trace Selected.")
 
     while True:
-        user_choice = session.prompt()
+        user_input = session.prompt()
 
 # Run the forward and inverse single-trace algorithms
 def forinv_single_trace():
 
-    print("Forward and Inverse Single-Trace Selected.")
+    print("Forward + Inverse Single-Trace Mode:")
 
+    # Run the prompt
     while True:
-        user_choice = session.prompt()
+
+        # Await under input
+        user_input = session.prompt()
+
+        # Check for 'go back' instruction
+        if user_input in BACK_COMMANDS:
+            return
+
+        # check for the help instruction
+        elif user_input == "dr":
+            print_driving_functions()
+
+        # Check if in the response correponds with help/exit
+        elif user_input in basic_responses:
+            basic_responses[user_input]("forinvsin")
+
+        # Check if a list of driving functions were entered
+        elif valid_driving_functions(user_input):
+            pass
+
+        # Print the bad input message
+        else:
+            bad_input_message(user_input)
 
 def two_trace():
 
     print("Two-Trace Selected.")
 
     while True:
-        user_choice = session.prompt()
+        user_input = session.prompt()
 
 def wedge_trace():
 
     print("Wedge Trace Selected.")
 
     while True:
-        user_choice = session.prompt()
+        user_input = session.prompt()
+
+# Run the inverse single-trace algorithm
+def root_mean_square():
+
+    print("Root Mean Square Mode:")
+
+    while True:
+        user_input = session.prompt()
 
 def main():
 
     # Create a dictionary of response-function pairs for the 'main' menu
-    algorithm_responses = { "forwardsingle" : forward_single_trace,
-                            "inversesingle" : inverse_single_trace,
-                            "forinvsingle" : forinv_single_trace,
+    algorithm_responses = { "forsin" : forward_single_trace,
+                            "invsin" : inverse_single_trace,
+                            "forinvsin" : forinv_single_trace,
                             "two" : two_trace,
                             "wedge" : wedge_trace,
+                            "rms" : root_mean_square,
                           }
 
     # Run the prompt
     while True:
 
         # Do multiple input calls.
-        user_choice = session.prompt()
+        user_input = session.prompt()
 
         # Check if in the response correponds with any of the Loewner algorithms
-        if user_choice in algorithm_responses:
-            algorithm_responses[user_choice]()
+        if user_input in algorithm_responses:
+            algorithm_responses[user_input]()
 
         # Check if in the response correponds with help/exit
-        elif user_choice in basic_responses:
-            basic_responses[user_choice](user_choice)
+        elif user_input in basic_responses:
+            basic_responses[user_input](user_input)
 
+        # Print a message if an invalid response was given
         else:
-            # Print a message if an invalid response was given
-            print("Unrecognised instruction: " + user_choice + " (Press h for help)")
+            bad_input_message(user_input)
 
 # Create prompt object.
 session = PromptSession(message=LOEWNER_PROMPT)
@@ -113,9 +171,9 @@ basic_responses = { "help" : print_help_message,
                   }
 
 # Create a dictionary of response-function pairs for the 'main' menu
-help_responses = { "forwardsingle" : FORSINGLE_HELPMSG,
-                   "inversesingle" : INVSINGLE_HELPMSG,
-                   "forinvsingle" : FORINV_HELPMSG,
+help_responses = { "forsin" : FORSINGLE_HELPMSG,
+                   "invsin" : INVSINGLE_HELPMSG,
+                   "forinvsin" : FORINV_HELPMSG,
                    "two" : TWO_HELPMSG,
                    "wedge" : WEDGE_HELPMSG,
                    "help" : HELPMSG,

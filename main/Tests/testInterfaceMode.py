edb4bbcb441ed2b64ee5573a0740dabacda4f179
ucapdak <ucapdak@ucl.ac.uk>
2018-08-27 22:11:42 +0100

    neater argument array

--- main/Tests/testInterfaceMode.py
+++ main/Tests/testInterfaceMode.py
@@ -7,11 +7,19 @@ class MyTest(unittest.TestCase):
 
     def test_initialise(self):
 
+        # Create an InterfaceMode object
         interface_mode = SingleTrace()
 
-        good_args = [("starttime","0"),("finaltime","3"),("outerres","3"),("innerres","5"),("saveplots","y"),("saveplots","n"),("savedata","y"), \
-                ("savedata","n"),("compile","y"),("compile","n"),("kappa","3.5"),("drivealpha","0.2")]
+        # Create a list of good arguments
+        good_args = ["starttime 0","finaltime 3","outerres 3","innerres 5","saveplots y","saveplots n","savedata y", \
+                "savedata n","compile y","compile n","kappa 3.5","drivealpha 0.2"]
 
         for arg in good_args:
-            self.assertEqual(interface_mode.change_single_parameter(arg[0],arg[1]),True)
+            self.assertEqual(interface_mode.change_single_parameter(*arg.split()),True)
+            self.assertEqual(interface_mode.change_parameters(arg),True)
+
+    def test_kappa(self):
+
+        kappa_forsin_mode = ForSinKappa()
+
 
